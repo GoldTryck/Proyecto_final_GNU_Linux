1643b5e73d130d94c985017a1a8bcb20d4c468e5
hector <lariosh69@gmail.com>
2023-09-22 02:23:46 -0600

    [ADD][HECTOR] Version final funcion bashmusic

--- ArchivosSH/bashmusic.sh
+++ ArchivosSH/bashmusic.sh
@@ -22,7 +22,7 @@ if ! command -v mpg123 &>/dev/null; then
     if [[ $opcion =~ ^[sS]$ ]]; then
         sudo apt install mpg123
         echo -e "${bold}${v}mpg123 se ha instalado correctamente. Puede utilizar el reproductor de música ahora.${normal}${b}"
-        sleep 5
+        sleep 2
     else
         echo -e "${bold}${am}No se ha instalado mpg123. No podrá utilizar el reproductor de música.${normal}${b}"
         exit 1
@@ -42,19 +42,20 @@ saludo () {
 }
 cambiar_dir(){
 	dir=()
-  	printf "Ingrese el directorio con archivos a reproducir (puede explorar con Tab): "
+  	echo -n "Ingrese el directorio con archivos a reproducir (puede explorar con Tab): "
   	read -e dir
   	cd "$dir"
   	echo -e "\n${bold}${v} El directorio seleccionado ahora es: $(pwd)${normal}${b}"
-  	sleep 5
+  	sleep 2
 }
 menu(){
 	opc=()
 	echo -e "\n\t${bold}${am}SELECCIONE LA OPCION DE REPRODUCCION DE SU PREFERENCIA:${normal}${b}\n\n"
 	show_opc "1)" "Reproducir una canción en el directorio seleccionado ($(pwd)).\n"
 	show_opc "2)" "Reproducir todas las canciones en el directorio seleccionado\n"
-	show_opc "3)" "Cambiar el directorio seleccionado\n"
-	show_opc "4)" "Salir\n"
+	show_opc "3)" "Reproducir todas las canciones en el directorio seleccionado (ALEATORIO)\n"
+	show_opc "4)" "Cambiar el directorio seleccionado\n"
+	show_opc "5)" "Salir\n"
 	printf "> "
 	read opc
 	return "$opc"
@@ -95,6 +96,42 @@ controles(){
     	echo -e "\t╚═══════════╩═════════════════════════════════╝${normal}${b}"
 	printf "\n🟩🟨🟧🟥🟪🟦🟩🟨🟧🟥🟪🟦🟩🟨🟧🟥🟪🟦🟩🟨🟧🟥🟪🟦🟩🟨🟧🟥🟪🟦\n"		
 }
+rep_cancion(){
+	cancion=()
+	if [ -z "$(ls *.mp3 2>/dev/null)" ]; then
+		echo -e "\n${r}No se encontraron archivos .mp3 en el directorio actual.${normal}${b}"
+  		sleep 2
+	else
+		echo -e "${bold}${m}\n”Seleccione la canción a reproducir (Explorar dir: [Tab]): ${normal}${b}"
+		ls *.mp3
+		echo -n "> "
+		read -e cancion
+		controles
+		mpg123 -C --title -q "$cancion"
+		printf "🟩🟨🟧🟥🟪🟦🟩🟨🟧🟥🟪🟦🟩🟨🟧🟥🟪🟦🟩🟨🟧🟥🟪🟦🟩🟨🟧🟥🟪🟦\n"
+		sleep 2
+	fi
+}
+rep_lista(){
+	local opc=$1
+	modo_rep=()
+	if [ "$opc" -eq 1 ]; then 
+		modo_rep=(-C --title -q -z ./*.mp3)
+	else 
+		modo_rep=(-C --title -q ./*.mp3)
+	fi 
+	if [ -z "$(ls *.mp3 2>/dev/null)" ]; then
+  		echo -e "\n${r}No se encontraron archivos .mp3 en el directorio actual.${normal}${b}"
+  		sleep 2
+	else
+  		echo -e "\n${t}Archivos .mp3 encontrados:${normal}${b}"
+  		ls *.mp3
+		controles
+		mpg123 "${modo_rep[@]}"
+		printf "🟩🟨🟧🟥🟪🟦🟩🟨🟧🟥🟪🟦🟩🟨🟧🟥🟪🟦🟩🟨🟧🟥🟪🟦🟩🟨🟧🟥🟪🟦\n"
+  		sleep 2
+	fi
+}
 reproductor(){
 	while true 
 	do
@@ -104,37 +141,20 @@ reproductor(){
 		salida_menu=$?
 		case $salida_menu in
 			1)
-				cancion=()
-				if [ -z "$(ls *.mp3 2>/dev/null)" ]; then
-  					echo -e "\n${r}No se encontraron archivos .mp3 en el directorio actual.${normal}${b}"
-  					sleep 5
-  					continue
-				else
-					echo -e "${bold}${m}\n”Seleccione la canción a reproducir: ${normal}${b}"
-					ls *.mp3
-					echo -n "> "
-					read -e caoncion
-					controles
-					sleep 10
-				fi
+				rep_cancion
 				;;
 			2)
-				if [ -z "$(ls *.mp3 2>/dev/null)" ]; then
-  					echo -e "\n${r}No se encontraron archivos .mp3 en el directorio actual.${normal}${b}"
-  					sleep 5
-  					continue
-				else
-  					echo -e "\n${t}Archivos .mp3 encontrados:${normal}${b}"
-  					ls *.mp3
-  					sleep 5
-				fi
+				rep_lista 0				
 				;;
 			3)
-				cambiar_dir
+				rep_lista 1
 				;;
 			4)
+				cambiar_dir
+				;;
+			5)
 				cd "$dir_script"
-        clear
+        		clear
 				exit 0
 				;;
 			*)
