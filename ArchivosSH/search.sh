4663f526f8e75ed1c65f8df8265b962b559e1405
hector <lariosh69@gmail.com>
2023-09-22 04:58:26 -0600

    [FIX][HECTOR] Correccion de declaracion de var .

--- ArchivosSH/search.sh
+++ ArchivosSH/search.sh
@@ -47,7 +47,7 @@ sec=${time[2]}
 hour=$(($hour+6))
 
 prompt () {
-    echo -ne "\n${az}╔╣ ${v}${bold}$(whoami)${b}${normal}@${t}${bold}$(hostname) ${normal}${az}╠══╣ ${m}${bold}$hour:$min:$sec ${normal}${az}╠══╣ ${b}$1 ${normal}${az}║\n${az}╚═╣ ${r}${bold}$ ${normal}${az}╠═${bold}${r}>${v}>${t}>${normal}${b} "
+    echo -ne "\n${az}╔╣ ${v}${bold}$(whoami)${b}${normal}@${t}${bold}$HOSTNAME ${normal}${az}╠══╣ ${m}${bold}$hour:$min:$sec ${normal}${az}╠══╣ ${b}$1 ${normal}${az}║\n${az}╚═╣ ${r}${bold}$ ${normal}${az}╠═${bold}${r}>${v}>${t}>${normal}${b} "
     read nombre
 }
 
